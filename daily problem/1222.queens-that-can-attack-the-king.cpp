1d839220fd419822494e1df7c09d38219a7921eb
Fxzbed <Fxzbed@outlook.com>
2023-09-14 20:03:42 +0800

    2023/9/14 - daily problem

---
+++ daily problem/1222.queens-that-can-attack-the-king.cpp
@@ -0,0 +1,18 @@
+#include "Fxzbed_algorithms.hpp"
+using namespace std;
+/*
+ * @lc app=leetcode id=1222 lang=cpp
+ *
+ * [1222] Queens That Can Attack the King
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king) {
+
+    }
+};
+// @lc code=end
+
+
