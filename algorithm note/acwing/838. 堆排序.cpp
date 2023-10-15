2d0a956e45315fc1f61cd17f38621c9f1b99c8ba
Fxzbed <fxzbed@FxzbeddeMini.lan>
2023-10-15 17:20:01 +0800

    2023/10/15 - note

---
+++ algorithm note/acwing/838. 堆排序.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+constexpr int N = 1e5 + 10;
+
+int h[N], edge, n, m;
+
+void down(int i) {
+    int t = i;
+    if (i * 2 <= edge && h[i * 2] < h[t]) t = i * 2;
+    if (i * 2 + 1 <= edge && h[i * 2 + 1] < h[t]) t = i * 2 + 1;
+    if (t != i) {
+        swap(h[t], h[i]);
+        down(t);
+    }
+}
+
+int main(void) {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i ++) scanf("%d", &h[i]);
+    edge = n;
+
+    for (int i = n / 2; i; i--) down(i);
+
+    while (m --) {
+        printf("%d ", h[1]);
+        h[1] = h[edge];
+        edge --;
+        down(1);
+    }
+
+    return 0;
+}
