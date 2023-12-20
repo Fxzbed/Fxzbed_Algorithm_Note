bd0794b431a0c4eb98d59c1e637705013ecec583
Fxzbed <Fxzbed@outlook.com>
2023-12-20 12:31:12 +0800

    2023/12/20 - daily problem

---
+++ daily problem/2828. Check if a String Is an Acronym of Words.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if (words.size() != s.size()) return false;
+        int idx = 0;
+        for (auto &str : words) {
+            if (str[0] != s[idx ++]) return false;
+        }
+        return true;
+    }
+};
